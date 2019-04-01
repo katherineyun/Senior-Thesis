fe028e0dc87f39848a88e061a21efb21a544a432
Kat <myun7@illinois.edu>
2019-04-01 15:41:19 -0500

    Completed motor control, need to finish actConvertor

--- main.py
+++ main.py
@@ -15,49 +15,50 @@ def main(env_id):
 
 	if env_id is 'icubEnv':
 
-
-		# ##initialize environment
+		##initialize environment
 		env = gym.make('icub-v0')
-		# speech = speechRec()
-		# recognizer, microphone = speech.init()
-		# word = speech.startGame(recognizer, microphone)
-		# if(word == False):
-		# 	return
-		# char_names = ["Rachel", "Joey", "Phoebe", "Catherine", "Monica","Chandler", "Ross"]
-		# art_names = ['jennifer_anniston', 'matt_leBlanc', 'lisa_kudrow', 'unknown', 'courteney_cox', 'matthew_perry', 'david_schwimmer']
-		#
-		# for i in range(len(char_names)):
-		# 	if(word == char_names[i]):
-		# 		target = art_names[i]
-		# 		break
 
-		target = 'jennifer_anniston'
+		##recognize speech
+		speech = speechRec()
+		recognizer, microphone = speech.init()
+		word = speech.startGame(recognizer, microphone)
+		if(word == False):
+			return
+		char_names = ["Rachel", "Joey", "Phoebe", "Catherine", "Monica","Chandler", "Ross"]
+		art_names = ['jennifer_anniston', 'matt_leBlanc', 'lisa_kudrow', 'unknown', 'courteney_cox', 'matthew_perry', 'david_schwimmer']
+
+		for i in range(len(char_names)):
+			if(word == char_names[i]):
+				target = art_names[i]
+				break
+
+		##decide target and face recognition
+		#target = 'courteney_cox'
 		encodings = "env/Gazebo/friends.pickle"
-		image = cv2.imread('env/Gazebo/00000054.jpg')
+		#image = cv2.imread('env/Gazebo/00000054.jpg')
 
+		#target_coor = [-0.41, 0.1, 0.5]
 
-		#icub_coor = actConverter(target_coor)
-
-		action = [0.41, -0.1, 0.96]
-		#for i_episode in range(20):
-		observ = env.reset()
-		time.sleep(3)
-
-
+		env.reset()
+		time.sleep(2)
 
 		image = env.cameraReader()
-		time.sleep(3)
+		time.sleep(2)
+
 		face = faceRec(encodings, image, target)
 		target_coor = face[0][1]
+		action = env.actConverter(target_coor)
 
-		for t in range(200):
+		for t in range(300):
 			env.step(action)
+
+		time.sleep(3)
 		env.close()
 
-	return target_coor
+
+	return "Simulation Ended"
 
 if __name__ == "__main__":
 	environment='icubEnv'
-	target_coor = main(environment)
-
-	print(target_coor)
+	signal = main(environment)
+	print(signal)
